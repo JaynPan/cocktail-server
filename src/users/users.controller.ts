77263443da1208feece0347b86c8aa32cd73aa40
Jay Pan <lb0104672@gmail.com>
2022-05-28 00:55:21 +0800

    complete CRUD at auth API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UpdateUserDto } from './dtos/updateUser.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -9,5 +19,25 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     this.UsersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.UsersService.findOne(id);
+  }
+
+  @Get()
+  findAllUser(@Query('email') email: string) {
+    return this.UsersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.UsersService.remove(id);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.UsersService.update(id, body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,4 +12,40 @@ export class UsersService {
 
     return this.repo.save(user);
   }
+
+  findOne(id: string) {
+    return this.repo.findOne({ where: { id } });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: string, payload: Partial<User>) {
+    // to ensure the entity hooks being trigger, should find the entity and save it
+    // it's a tradeoff since it's a little bit efficient
+    // !return this.repo.update(id, payload);
+
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('user not found');
+    }
+
+    Object.assign(user, payload);
+    return this.repo.save(user);
+  }
+
+  async remove(id: string) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('user not found');
+    }
+
+    return this.repo.remove(user);
+
+    // !delete won't trigger hooks
+    // return this.repo.delete(id);
+  }
 }
