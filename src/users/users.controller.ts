dcf365b673ff68672b1ef5448d64213342a3460a
Jay Pan <lb0104672@gmail.com>
2022-05-30 00:27:22 +0800

    NestJS recommend data serialization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,8 @@ import {
   Patch,
   Post,
   Query,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UpdateUserDto } from './dtos/updateUser.dto';
@@ -22,6 +24,7 @@ export class UsersController {
     this.UsersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.UsersService.findOne(id);
