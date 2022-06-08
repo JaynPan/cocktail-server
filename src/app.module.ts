ecda34d0c911e9340f801c8336eb19a9f0d41add
Jay Pan <lb0104672@gmail.com>
2022-06-08 22:33:39 +0800

    test: add e2e test at signup req  and tranform main.ts setup to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,6 +8,9 @@ import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
@@ -19,6 +24,27 @@ import { Report } from './reports/report.entity';
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // transform the global pipe setup from main.ts to app.module.ts
+    // to make e2e testing works
+    // applying every incoming request to this application, running through  useValue instance
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['asdufi'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const EMAIL = 'example1@gmail.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email: EMAIL,
+        password: '123456',
+      })
+      .expect(201)
+      .then((res) => {
+        const { email, id } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(EMAIL);
+      });
+  });
+});
